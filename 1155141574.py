f39e5f17064564a97432f1bce823080371e1e7e3
I1amanoutsider <62247980+I1amanoutsider@users.noreply.github.com>
2020-09-23 11:06:31 +0800

    Create 1155141574.py (#6)

---
+++ 1155141574.py
@@ -0,0 +1,22 @@
+#exercise1
+'''height = 1.75
+weight = 80.5
+bmi = weight/(height)*height
+if (18.5<= bmi and bmi<=25):
+    print("healthy")
+else:
+    print("unhealthy")'''
+#exercise2
+'''ex2num = input()
+ex2num_inverse = ex2num[::-1]
+print(ex2num_inverse)
+if (ex2num == ex2num_inverse):
+    print("The original and reverse number is the same")
+else:
+    print("The original and reverse number is not the same")'''
+#exercise3
+'''list = [1,3,4,8,9]
+print(sum(list))'''
+
+
+
